c62080b5e6df33f48f0b2ecb2b1dfe719debbe74
2laniGuwela <guyulonene@gmail.com>
2024-09-05 03:45:34 +0200

    Aviotor
    2lani tools

---
+++ script.js
@@ -0,0 +1,10 @@
+function simulategame(){const rounds =Document. getElementById('rounds').value; 
+    const crashpointsElementById(crashpoints)
+;crashpointsElementById.innerHTML=//clear previous result
+const crashpoints=[];for(let i=0;i<round i++){//generate a random crash multiplier between 1.0x and 300x
+    const crashpoint=(math.random()*(100-1)+1).toFixed(2);
+crashpoints.push(crashpoint)}
+crashpoints.forEach(point=>{conti 1i=document.createElement('1i);
+    1i.textcontent =$(points')x;
+        crashpointsElement. appendChild(1i)})
+    }
